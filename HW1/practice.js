255853f00a16c57e8abf2fa06f45ac4fd8fe2e10
igors <igor.stolenko@gmail.com>
2024-01-25 15:59:41 +0200

    homework 1

---
+++ HW1/practice.js
@@ -0,0 +1,27 @@
+console.log(typeof 'Dmitro');
+console.log(typeof 5687);
+dum = null
+console.log(typeof dum);
+console.log(typeof dun);
+console.log(typeof 8726587465876n);
+console.log(typeof Symbol('Halah'));
+can = true
+console.log(typeof can);
+
+console.log(3==3);
+console.log(3==4);
+console.log(3=='3');
+console.log("3"=='3');
+
+console.log(3===3);
+console.log(3===4);
+console.log(3==='3');
+console.log("3"==='3');
+
+a=1;b=2;c='1';d=1
+console.log(a===b)
+console.log(a==c)
+console.log(a===d)
+console.log(b==c)
+
+null == undefined  
